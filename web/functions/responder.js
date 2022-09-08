f9784e91fd4f738ed21f62dab72c3c46dfde58c8
Willson Smith <me@willsonsmith.com>
2022-09-08 17:52:57 -0400

    changes

---
+++ web/functions/responder.js
@@ -0,0 +1,16 @@
+import { WebClient } from "@slack/web-api";
+const wc = new WebClient(process.env.SLACK_TOKEN);
+export async function handler(requestEvent) {
+  const body = JSON.parse(requestEvent.body);
+  // const {event} = body;
+  // const {token, challenge, type, event} = body;
+
+  if (body.event) {
+    const { channel } = body.event;
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ hello: `world` }),
+  };
+}
